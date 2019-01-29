573aa312a831c7f4362549c793233eea7b53141d
WillFoster95 <w.foster5764@student.leedsbeckett.ac.uk>
2019-01-29 14:05:41 +0000

    Camera Zoom

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,40 +3,33 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour
 {
-    private new GameObject camera;
+    public Camera m_OrthographicCamera;
+    
     void Update()
     {
-        camera = GameObject.Find("Main Camera");
-
-        float c_Position = camera.transform.position.y;
-        float speed = c_Position * 0.04f;
+        float zoom = 0f;
+        zoom = Input.GetAxis("Mouse ScrollWheel");
+        m_OrthographicCamera.orthographicSize -= zoom * 2;
+                
+        Vector3 p_Velocity = new Vector3();       
         
-        Vector3 p_Velocity = new Vector3(0, 0, 0);
-        
-        Vector3 p_Zoom = new Vector3();        
-        int zoom_Speed = 10;
-    
-        float zoom = Input.GetAxis("Mouse ScrollWheel");
-        p_Zoom = new Vector3(0, 0, zoom * zoom_Speed * speed);
-
         if (Input.GetKey(KeyCode.W))
         {
-            p_Velocity += new Vector3(0, 0, 1 * speed);
+            p_Velocity += new Vector3(0, 1, 0);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            p_Velocity += new Vector3(0, 0, -1 * speed);
+            p_Velocity += new Vector3(0, -1, 0);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            p_Velocity += new Vector3(-1 * speed, 0, 0);
+            p_Velocity += new Vector3(-1, 0, 0);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            p_Velocity += new Vector3(1 * speed, 0, 0);
+            p_Velocity += new Vector3(1, 0, 0);
         }
         
-        transform.Translate(p_Zoom);
         transform.Translate(p_Velocity, Space.World);
     } 
 }
