a10aa0aaaf38e90845b9661d2401d651a894263f
GarethLeckey <g.leckey1996@gmail.com>
2019-02-18 18:13:36 +0000

    Created boundary around template map (playArea)

--- Assets/Scripts/Hare_Movement.cs
+++ Assets/Scripts/Hare_Movement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Hare_Movement : MonoBehaviour
 {
     public float moveSpeed;
+    private Vector2 minWalkPoint;
+    private Vector2 maxWalkPoint;
 
     private Rigidbody2D myRigidBody;
 
@@ -16,6 +18,12 @@ public class Hare_Movement : MonoBehaviour
     private float waitCounter;
 
     private int walkDirection;
+
+    //Will hopefully be able to develop into DEN system
+    public Collider2D playArea;
+    private bool hasplayArea;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +31,15 @@ public class Hare_Movement : MonoBehaviour
 
         waitCounter = walkTime;
         walkCounter = walkTime;
+
+        ChooseDirection();
+
+        if (playArea != null)
+        {
+            minWalkPoint = playArea.bounds.min;
+            maxWalkPoint = playArea.bounds.max;
+            hasplayArea = true;
+        }
     }
 
     // Update is called once per frame
@@ -36,18 +53,38 @@ public class Hare_Movement : MonoBehaviour
             {
                 case 0:
                     myRigidBody.velocity = new Vector2(0, moveSpeed);
+                    if (hasplayArea && transform.position.y > maxWalkPoint.y)
+                    {
+                        isWalking = false;
+                        waitCounter = waitTime;
+                    }
                     break;
 
                 case 1:
                     myRigidBody.velocity = new Vector2(moveSpeed, 0);
+                    if (hasplayArea && transform.position.x > maxWalkPoint.x)
+                    {
+                        isWalking = false;
+                        waitCounter = waitTime;
+                    }
                     break;
 
                 case 2:
                     myRigidBody.velocity = new Vector2(0, -moveSpeed);
+                    if (hasplayArea && transform.position.y < minWalkPoint.y)
+                    {
+                        isWalking = false;
+                        waitCounter = waitTime;
+                    }
                     break;
 
                 case 3:
                     myRigidBody.velocity = new Vector2(-moveSpeed, 0);
+                    if (hasplayArea && transform.position.x < minWalkPoint.x)
+                    {
+                        isWalking = false;
+                        waitCounter = waitTime;
+                    }
                     break;
 
             }
