3c16976d4080166e52a5e108c792b57b215c24b4
GarethLeckey <g.leckey1996@gmail.com>
2019-02-18 17:45:14 +0000

    Beginning development of animal movement(Bear/Hare)

---
+++ Assets/Scripts/Hare_Movement.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hare_Movement : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private Rigidbody2D myRigidBody;
+
+    public bool isWalking;
+
+    public float walkTime;
+    private float walkCounter;
+    public float waitTime;
+    private float waitCounter;
+
+    private int walkDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+
+        waitCounter = walkTime;
+        walkCounter = walkTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isWalking)
+        {
+            walkCounter -= Time.deltaTime;
+
+            switch (walkDirection)
+            {
+                case 0:
+                    myRigidBody.velocity = new Vector2(0, moveSpeed);
+                    break;
+
+                case 1:
+                    myRigidBody.velocity = new Vector2(moveSpeed, 0);
+                    break;
+
+                case 2:
+                    myRigidBody.velocity = new Vector2(0, -moveSpeed);
+                    break;
+
+                case 3:
+                    myRigidBody.velocity = new Vector2(-moveSpeed, 0);
+                    break;
+
+            }
+
+            if (walkCounter < 0)
+            {
+                isWalking = false;
+                waitCounter = waitTime;
+            }
+        }
+
+        else
+        {
+            waitCounter -= Time.deltaTime;
+
+            myRigidBody.velocity = Vector2.zero;
+
+            if (waitCounter < 0)
+            {
+                ChooseDirection();
+            }
+        }
+    }
+
+    public void ChooseDirection()
+    {
+        walkDirection = Random.Range(0, 4);
+        isWalking = true;
+        walkCounter = walkTime;
+    }
+
+}
