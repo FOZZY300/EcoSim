daf051bc56a0e9d7a6b5a7ffb76cbcf894448509
WillFoster95 <w.foster5764@student.leedsbeckett.ac.uk>
2019-03-05 16:37:34 +0000

    Move scripts to scripts folder

---
+++ Assets/Scripts/Test_Hare.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Hare : MonoBehaviour
+{   
+    //Test
+    private string animalName = "Hare", animalSex = "M", animalAge = "3";
+    private float hungerLevel = 0.2f, tirednessLevel = 0.2f, thirstLevel = 0.2f;
+    private int animalID = 2, boxNum = 8;
+    //Test
+
+    void Start()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        boxNum = Info_Panel.OpenBox(hungerLevel, tirednessLevel, thirstLevel, animalName, animalAge, animalSex, animalID);
+        Debug.Log("Hare boxNum: " + boxNum);
+    }
+
+    
+    private void FixedUpdate()
+    {
+              
+        
+    }
+    
+}
