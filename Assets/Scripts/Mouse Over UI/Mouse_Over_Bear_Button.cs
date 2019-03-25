4f3464b7d2ca5d4b7008c9d532389b504bf4e893
WillFoster95 <w.foster5764@student.leedsbeckett.ac.uk>
2019-03-25 12:22:44 +0000

    Updated bear info in more info panel

--- Assets/Scripts/Mouse Over UI/Mouse_Over_Bear_Button.cs
+++ Assets/Scripts/Mouse Over UI/Mouse_Over_Bear_Button.cs
@@ -13,7 +13,17 @@ public class Mouse_Over_Bear_Button : MonoBehaviour , IPointerEnterHandler, IPoi
     {
         Debug.Log("Mouse on bear button");
         moreInfo.SetActive(true);
-        moreInfoText.text = "Bear";
+        moreInfoText.text = "Common Name: Grizzly Bear \n" +
+            "Scientific Name: Ursus Arctos Horribilis \n" +
+            "Type: Mammal \n" +
+            "Diet: Omnivore \n" +
+            //"moose, deer, sheep, elk, bison, caribou, various rodents and even black bears.\n" +
+            //"Fish: salmon, bass and trout.\n Plants: pine nuts, sedges, bulbs, roots, berries, grasses." +
+            "Lifespan: 20 - 25 \n" +
+            //"Hibernate: 5 - 8 Months \n " +
+            //"Size: head to tail, 8ft tall \n" +
+            "Weight: 400 to 600 lbs \n" +
+            //"Speed: 25mph \n";
     }
 
     public void OnPointerExit(PointerEventData eventData)
